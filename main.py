90c8ca653c54cd878f8656cb2cb39e20409a155b
artykbayevk <artykbayevk@gmail.com>
2019-06-17 17:37:51 +0600

    optimal solutions for images

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import cv2
 import numpy as np
 
+global base_path
+base_path = "data\\test\\"
+
+
 img1_path = "data\\test\\test_1.jpg"
 img2_path = "data\\test\\test_2.jpg"
 
@@ -29,8 +33,10 @@ def rootSIFT(img_path):
     (kps, descs) = sift.detectAndCompute(gray, None)
     rs = RootSIFT()
     (kps, descs) = rs.compute(gray, kps)
-
+    img = cv2.drawKeypoints(gray, kps, None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imwrite(base_path+'test_kp_'+img_path.split("_")[1], img)
     return kps,descs
 
 #%%
-
+a,b = rootSIFT(img1_path)
+a,b = rootSIFT(img2_path)
