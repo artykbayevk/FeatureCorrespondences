f9f4ff5d6b255ab28149fd4a2a2785ab9360f550
artykbayevk <artykbayevk@gmail.com>
2019-10-13 19:28:03 +0600

    todo done

--- DL/keras/main.py
+++ DL/keras/main.py
@@ -15,3 +15,11 @@ dummy_mult = 6
 train_data, val_data, test_data,size_of_sample = get_divided_data(path=path, val_size=val_size, ts_size=test_size, dummy_mult=dummy_mult)
 
 train_val(train_data=train_data, val_data=val_data, n_epochs=n_epochs, lr=lr, batch_size=batch_size, size_of_sample=size_of_sample)
+
+#TODO create folder with subfolders of each pairs: folder consists of *.csv files with X-Y points
+
+#TODO write dataloader, that download all feature correspondences into one dataset
+
+#TODO test created dataset with trained DL model
+
+#TODO evaluate each predicting
