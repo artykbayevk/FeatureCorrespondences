cefcb34d6a59fadb8ee208f79d8681b675839136
artykbayevk <artykbayevk@gmail.com>
2019-10-21 17:18:04 +0600

    points created, distances found. next step - generate solution

--- artificial_data.py
+++ artificial_data.py
@@ -1,18 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
-from scipy.spatial.distance import euclidean
+from scipy.spatial.distance import cdist
 from itertools import product
 
 
 class Dataset:
-    def __init__(self, max_value, min_value, number_of_p, number_of_q):
+    def __init__(self, max_value, min_value, number_of_p, number_of_q, samples = 1, d_of_p = 0.5):
         self.max_value = max_value
         self.min_value = min_value
         self.p_number = number_of_p
         self.q_number = number_of_q
+        assert self.q_number / self.p_number >= 3
 
-    def figure(self, origin_x, origin_y, ellipse, radius_x, radius_y, angle, plot_figure=True):
+        self.ellipse = np.linspace(0, 2 * pi, self.q_number)
+        self.origin_x = int(self.max_value / 2)
+        self.origin_y = int(self.max_value / 2)
+        self.samples = samples
+
+
+
+        self.radius_on_x = 1.0
+        self.radius_on_y = 0.5*int(self.p_number/2)+self.radius_on_x
+        self.d_of_p = d_of_p
+
+    def figure(self, angle, plot_figure=True):
         """
 
         :param origin_x:
@@ -24,9 +36,36 @@ class Dataset:
         :param plot_figure:
         :return: p_x, p_y, q_x, q_y - 4 arrays of points of P and Q figures
         """
-        return self.p_number + self.q_number
+        d = self.d_of_p
+        p_n = self.p_number
 
-    def get_distances(self, p_x, p_y, q_x, q_y, norm_value):
+        p_y,p_x = np.arange(-d*int(p_n/2), d*int(p_n/2) + d, d) + self.origin_x, np.zeros(p_n)+self.origin_x
+
+        q_x = self.origin_x + self.radius_on_x * np.cos(self.ellipse)
+        q_y = self.origin_y + self.radius_on_y * np.sin(self.ellipse)
+
+        if plot_figure:
+            plt.scatter(q_x, q_y, label="Q")
+            plt.scatter(p_x, p_y, label="P")
+
+            plt.xlabel("X")
+            plt.ylabel("Y")
+
+            plt.axis('equal')
+            plt.grid(color='lightgray', linestyle='--')
+            plt.legend('QP', loc='upper left')
+            plt.show()
+
+        P = np.array([
+            p_x,p_y
+        ])
+        Q = np.array([
+            q_x,q_y
+        ])
+
+        return P.T,Q.T
+
+    def get_distances(self, P,Q):
         """
 
         :param p_x:
@@ -36,6 +75,8 @@ class Dataset:
         :param norm_value:
         :return: dict of pairs
         """
+        dist = cdist(P,Q,) <= 1.0
+        stop = 1
         return self.min_value
 
     def get_solutions(self, pairs):
@@ -53,3 +94,21 @@ class Dataset:
         :return:
         """
         return sol + self.q_number
+
+    def generate(self):
+        angle = np.pi
+        P, Q = self.figure(angle=angle, plot_figure=False)
+        dist = self.get_distances(P,Q)
+
+    def __str__(self):
+        return "Figure with P:{} and Q:{}\nOrigin Point: {}:{}\nRadius on X:{} and radius on Y:{}".format(
+            self.p_number, self.q_number, self.origin_x, self.origin_y, self.radius_on_x, self.radius_on_y
+        )
+
+dataset = Dataset(
+    max_value=100,
+    min_value=0,
+    number_of_p=13,
+    number_of_q=41)
+print(dataset)
+dataset.generate()
